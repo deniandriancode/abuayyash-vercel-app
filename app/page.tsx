ba9a41beecb0059b12e7a4d73010b67e9433a224
deniandriancode <deniandriancode@gmail.com>
2024-06-19 21:00:55 +0700

    auto update using script

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <section className="hero grid justify-center md:justify-start content-center px-2 md:max-w-[867px] md:px- md:mx-auto">
           <div className="md:ps-10">
             <h1 className="hero-title text-5xl md:text-6xl font-black mb-2">Abu Ayyash</h1>
-            <p className="opacity-[.74]">Ideas are cheap. Execution is expensive</p>
+            <p className="opacity-[.74]">Ideas are cheap. Execution is expensive.</p>
           </div>
         </section>
       </main>
