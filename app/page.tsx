770894b7bb9c0f95e6dcf7837934adb9ba02cdbe
deniandriancode <deniandriancode@gmail.com>
2024-06-19 16:36:07 +0700

    auto update using script

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import "./home.css";
 export default function Home() {
     return (
 	<div className="mx-auto md:container">
-	    <section className="hero grid justify-center md:justify-start md:grid-cols-2 content-center px-2 md:max-w-screen-md md:px-10 md:mx-auto">
-	    	<div>
+	    <section className="hero grid justify-center md:justify-start content-center px-2 md:max-w-[867px] md:px- md:mx-auto">
+	    <div className="md:ps-8">
 	    	<h1 className="hero-title text-5xl md:text-6xl font-black">Abu Ayyash</h1>
 	    	<p className="opacity-[.75]">Idea is cheap, execution is expensive</p>
 	    	</div>
