4e9d006048037f9e2ed8ec04f198ebae75415b9b
deniandriancode <deniandriancode@gmail.com>
2024-06-19 21:01:52 +0700

    auto update using script

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
         </nav>
       </header>
       { menuOpen && 
-      <div className="w-[50%] max-w-[200px] mt-2 px-2 py-2 absolute right-2 grid justify-end items-end text-right gap-1 md:hidden rounded border-[1px] border-[#b5b5b5]">
+      <div className="w-[50%] max-w-[200px] mt-2 px-2 py-2 absolute z-[10] right-2 grid justify-end items-end text-right gap-1 md:hidden rounded border-[1px] border-[#b5b5b5]">
         <Link className="bg-gray-0 hover:bg-gray-100 rounded px-2 transition-all" href="#"><IoSearchOutline className="inline" /></Link>
         <Link className="bg-gray-0 hover:bg-gray-100 rounded px-2 transition-all" href="#">Artikel</Link>
         <Link className="bg-gray-0 hover:bg-gray-100 rounded px-2 transition-all" href="#">Tentang</Link>
