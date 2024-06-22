ea7039b81aa0168649c7042b173495fd96066601
deniandriancode <deniandriancode@gmail.com>
2024-06-22 22:39:42 +0700

    auto update using script

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
         </nav>
       </header>
       { menuOpen && 
-      <div className="w-fit mt-2 ps-3 pe-2 py-3 absolute z-[10] right-2 flex flex-col justify-center items-end text-right gap-1 md:hidden rounded border-[1px] border-[#b5b5b5]">
+      <div className="w-fit bg-white mt-2 ps-3 pe-2 py-3 absolute z-[10] right-2 flex flex-col justify-center items-end text-right gap-1 md:hidden rounded border-[1px] border-[#b5b5b5]">
         <Link className="bg-gray-0 hover:bg-gray-100 hover:border-gray-300 border-transparent border-[1px] rounded px-2 transition-all inline" href="/search"><IoSearchOutline className="inline" /></Link>
         <Link className="bg-gray-0 hover:bg-gray-100 hover:border-gray-300 border-transparent border-[1px] rounded px-2 transition-all inline" href="/artikel">Artikel</Link>
         <Link className="bg-gray-0 hover:bg-gray-100 hover:border-gray-300 border-transparent border-[1px] rounded px-2 transition-all inline" href="/imtihan">Imtihan</Link>
